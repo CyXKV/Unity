085390f837b93ed28cdda0b5a3f8247c2f55f2d3
CyXKV <112751116+CyXKV@users.noreply.github.com>
2023-09-14 16:44:31 +0200

    Сделяль террейн

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -9,6 +9,7 @@ public class MovementPlayer : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     public Transform cam;
+    public float gravity = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +28,6 @@ public class MovementPlayer : MonoBehaviour
             Vector3 MoveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             character.Move(MoveDir.normalized* speed * Time.deltaTime);
-            
         }
     }
 }
